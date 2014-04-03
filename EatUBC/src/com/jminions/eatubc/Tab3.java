25b933eb5456232aaef874790648f0e046a1e857
g18-kc <kerbychang@gmail.com>
2014-04-03 16:55:41 -0700

    Added network

--- EatUBC/src/com/jminions/eatubc/Tab3.java
+++ EatUBC/src/com/jminions/eatubc/Tab3.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 public class Tab3 extends Fragment {
 	
+	public final static String EXTRA_MESSAGE = "com.jminions.eatubc.MESSAGE";
 	StringBuilder strb = new StringBuilder("");
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,8 +33,11 @@ public class Tab3 extends Fragment {
 					TextView temp = new TextView(getActivity());
 					TextView amount = new TextView(getActivity());
 					temp.setText(TabsInitActivity.arrChildElements[indexi][indexj]);
+					strb.append(TabsInitActivity.arrChildElements[indexi][indexj]+"\n");
 					amount.setText("Amount:" + TabsInitActivity.amountOrdered[indexi][indexj]
 							+ "\n-----------------------------------");
+					strb.append("Amount:" + TabsInitActivity.amountOrdered[indexi][indexj]
+							+ "\n-----------------------------------\n");
 					price_tab.addView(temp);
 					price_tab.addView(amount);
 					temp.setWidth(100);
@@ -46,15 +50,20 @@ public class Tab3 extends Fragment {
 		final TextView price = new TextView(getActivity());
 		price.setText("Total Price: $" + String.valueOf(TabsInitActivity.Price
 				+ "\n-----------------------------------"));
+		strb.append("Total Price: $" + String.valueOf(TabsInitActivity.Price
+				+ "\n-----------------------------------\n"));
 		price_tab.addView(price);
 		price.setWidth(100);
 		order_tab.addView(price_tab);
+		
+		
 		Button btnCancel = new Button(getActivity()); 
 	    btnCancel.setText("Cancel Order"); 
 	    order_tab.addView(btnCancel); 
 	    btnCancel.setOnClickListener(new Button.OnClickListener() {  
         public void onClick(View v)
             {
+        	
         	for (int indexj = 0; indexj < 3; indexj++) {
     			for (int indexi = 0; indexi < 2; indexi++) {
     				TabsInitActivity.amountOrdered[indexi][indexj] = 0;
@@ -67,24 +76,24 @@ public class Tab3 extends Fragment {
 //        	order_tab.removeView(amount);
         	//order_tab.removeAllViews();
         	order_tab.invalidate();
-
             }
          });
-		
-		
-		Button btn = new Button(getActivity()); 
-	    btn.setText("Place Order"); 
-	    order_tab.addView(btn); 
-	    
-	    
+
+
+		Button btnPlace = new Button(getActivity()); 
+	    btnPlace.setText("Place Order"); 
+	    order_tab.addView(btnPlace); 
+	    btnPlace.setOnClickListener(new Button.OnClickListener() {  
+	        public void onClick(View v)
+	            {
+	        	Intent intent = new Intent(getActivity(), FoodMenuActivity.class);
+	        	String message = strb.toString();
+	        	intent.putExtra(EXTRA_MESSAGE, message);
+	        	startActivity(intent);
+	            }
+	         });
+
+
 		return order_tab;
-	}
-	public void PlaceOrderClick(View view){
-		//Do this when user clicks Place Order Button
-		LinearLayout order_final = new LinearLayout(getActivity());
-		TextView final_order = new TextView(getActivity());
-		final_order.setText(strb);
-		order_final.addView(final_order);
-		
 	}
 }

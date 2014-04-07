be06283d29f4cb4512ca05c84b6f039fc6c65ac4
Jerry Huang <jerryhuang0123@hotmail.com>
2014-04-06 20:08:42 -0700

    added some functions, not yet merged

---
+++ EatUBC/src/com/jminions/eatubc/MainMenu.java
@@ -0,0 +1,45 @@
+package com.jminions.eatubc;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+public class MainMenu extends Activity{
+
+	//Intent intent = new Intent(this, DisplayOrderActivity.class);
+	public final static String EXTRA_MESSAGE = "com.jminions.eatubc.MESSAGE";
+
+	EditText order_line_1, order_line_2, order_line_3, order_line_4;
+
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void OnOrderClick(View view) {
+
+		Intent intent = new Intent(this, RestaurantList.class);
+		startActivity(intent);	 
+
+	}
+
+}

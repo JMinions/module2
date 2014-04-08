c9940f2ee24959f06b5221d986b3b94063b37e59
andy820316 <andy_lin1993@hotmail.com>
2014-04-08 00:47:39 -0700

    trying to merge

--- EatUBC/src/com/jminions/eatubc/FacebookLogin.java
+++ EatUBC/src/com/jminions/eatubc/FacebookLogin.java
@@ -8,28 +8,23 @@ import java.net.MalformedURLException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.facebook.android.AsyncFacebookRunner;
-import com.facebook.android.DialogError;
-import com.facebook.android.Facebook;
-import com.facebook.android.AsyncFacebookRunner.RequestListener;
-import com.facebook.android.Facebook.DialogListener;
-import com.facebook.android.FacebookError;
-
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-
 import android.view.View;
-
 import android.widget.Button;
-
 import android.widget.ImageView;
-
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
 
 public class FacebookLogin extends Activity{
 	
